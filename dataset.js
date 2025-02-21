af336bfe4b80b1edb507f9b522b9edc7a293c178
dmsgur <dmsgur@gmail.com>
2025-02-21 11:07:58 +0900

    1

--- dataset.js
+++ dataset.js
@@ -26,9 +26,18 @@ d1.set_img(0,{imgtitle:"모델의 예측",imgurl:"https://drive.google.com/file/
 
 
 
-d1.set_content("목표 시스템 개발을 위한 적정한 언어를 선정하고 라이선스와 향후 지원사항을 참고하여 결정한다.")
-d1.set_img(1,{imgtitle:"개발언어선정",imgurl:"https://drive.google.com/file/d/15GgAeFgW1Cxctk9PbJagUNfG2UkEU4WS/view?usp=drive_link",imglog:"자바언어기반 스프링프레임워크, html5, memberData file",sourceurl:"https://docs.google.com/document/d/1UvD1fZNL9yjLORwxgAW5vD6C-A_RwL1zcNan9RZqmys/edit?usp=drive_link"})//이미지타이틀
-d1.set_img(1,{imgtitle:"스프링프레임워크",imgurl:"https://drive.google.com/file/d/1HRUQhj2Tk6AzIW_NSRD59wGUAduJYTmW/view?usp=drive_link",imglog:"오픈소스 스프링프레임워크 v4.221",sourceurl:""})//이미지타이틀
+d1.set_content("당뇨환자에 대한 1년 상태 예측, 예측값은 연속데이터로 출력")
+d1.set_img(1,{imgtitle:"샘플소스코드",imgurl:"https://drive.google.com/file/d/1pwNqgKksSjmjEQMZweU6znZufSmd37kS/view?usp=drive_link",imglog:"당뇨환자에 대한 나이, 성별, bmi수치, bp수치등을 10개의 항목을 측정하여 1년후 당뇨수치에 대한 증감데이터 ",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"소스데이터수신 및 분석",imgurl:"https://drive.google.com/file/d/1Ix_aL2S8iNR5PWyqbPVky3d37r7SPqxy/view?usp=drive_link",imglog:"sklearn의 diabetes 데이터를 수신하여 항목별 분석과 수치값과 1년후 변화 질량에 대한 확인",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"연관성분석을 위한 산점도",imgurl:"https://drive.google.com/file/d/1K4rP9avRKdyFA3LsYARPuBsqQPZwsxge/view?usp=drive_link",imglog:"1년후 질량과 측정된 수치의 연관성 분석을 위한 산점도 그래프 확인",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"1.선택데이터",imgurl:"https://drive.google.com/file/d/1S46qFh38vPj7exySU-0XfUqqvVTZdZmn/view?usp=drive_link",imglog:"bmi 수치는 대각방향의 선형형태를 띄고 있어 1년후 질량과 연관이 있는 데이터로 판명",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"2.선택데이터",imgurl:"https://drive.google.com/file/d/1qLuGLo66PC13sBWp5OxKRl3JpP8Fxt6V/view?usp=drive_link",imglog:"s5 수치는 대각방향의 선형성을 띄고 있어 1년후 질량과 연관이 있는 데이터로 판명",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"3.미선택데이터",imgurl:"https://drive.google.com/file/d/1prRUEB8jO_OvDIxqv2nxGI-CCPT5U-GC/view?usp=drive_link",imglog:"성별데이터는 방향의 선형성이 없으므로 1년후 질량과 연관이 없음으로 판명되어 분석에서 제외",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"데이터추출",imgurl:"https://drive.google.com/file/d/13lOi4VQ2InDALG3NDnlHtEQmNkGUKl9d/view?usp=drive_link",imglog:"선택된 2번 8번 인덱스 데이터 추출 및 데이터의 모양 확인후 문제데이터와 정답데이터 값 확인분석",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"모델구성과 훈련",imgurl:"https://drive.google.com/file/d/1V4Op_0gCezDqBX17pe1v8NgIb9Gv3huq/view?usp=drive_link",imglog:"순차모델구성과 입출력 레이 구성, MSE 손실함수와 SGD를 최적화함수이 사용하여 컴파일 및 훈련 13회 실행",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"훈련결과시각화",imgurl:"https://drive.google.com/file/d/1fCA5YNzcyPG7E98kGFBlUyG_1OCRZwdM/view?usp=drive_link",imglog:"100회 이상의 훈련후 시각그래프 판단하에 12회 훈련이 최적화로 판단되어 12회 훈련으로 조기종료함.",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+d1.set_img(1,{imgtitle:"데이터 예측",imgurl:"https://drive.google.com/file/d/1OzI4CLllDVi6_HtUQE6Ojjzh2SpEjrqu/view?usp=drive_link",imglog:"테스트 데이터 10개를 일부 추출하여 예측 실행, 결과는 오차확률 4% 수치대로 접근",sourceurl:"https://github.com/dmsgur/Regression_250220/blob/codeone/LinearRegression/diabet.py"})//이미지타이틀
+
 
 
 d1.set_content("코딩개발도구와 테스트 및 빌드, 패키징, 배포등 개발 전반에 대한 필요한 환경을 설정한다.")
